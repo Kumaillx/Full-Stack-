795a1968f5864aac36e01115d0dada59592bb1e2
Syed M.Kumail Ali <101238514+bsee21068@users.noreply.github.com>
2025-01-02 01:27:52 +0500

    Use of Props In App JSX File

--- Part-1/Vite-demo/src/App.jsx
+++ Part-1/Vite-demo/src/App.jsx
@@ -1,22 +1,26 @@
+const Hello = (props) => {
+  console.log(Hello)
+    return (
+      <div>
+      <p> Hello {props.name}, You are {props.age} years old </p>
+      </div>
+    )
+}
+
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now,a+b)  
-  return (
+
+  const name = 'Yousuf'
+  const age = 23
+
+  return(
 
     <div>
-      <p> Hey this is my demo Vite File {now.toString()}</p>
-      <p>Name is Kumail</p>
-
-      <p>
-        {a} plus {b} is {a+b}
-      </p>
-
+      <h1> Greetings</h1>
+        <Hello name='Kumail' age ={10+12} />
+        <Hello name={name} age = {age} />
+        
     </div>
   )
-
-
 }
 
 export default App
