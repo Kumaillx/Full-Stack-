77fc48382987a465f358dd986fca6fae37701260
Syed M.Kumail Ali <101238514+bsee21068@users.noreply.github.com>
2025-01-10 23:44:22 +0500

    Re-Rendering

--- Part-1c/src/main.jsx
+++ Part-1c/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from './App'
+
+let counter = 1
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const refresh = () => {
+
+  root.render(
+    <App counter={counter}/>
+  )
+}
+
+refresh()
+counter += 1
+refresh()
+counter += 1
+refresh()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App counter={counter} />
 )
