d5cbbce1355b5adcc14877eaff0e8451e5ce8510
Syed M.Kumail Ali <101238514+bsee21068@users.noreply.github.com>
2025-01-14 00:30:41 +0500

    State and event handlers in JSX

--- Part-1c/src/App.jsx
+++ Part-1c/src/App.jsx
@@ -1,8 +1,29 @@
-const App = (props) => {
-  const {counter} = props
+import { useState } from 'react'
+
+//An event handler should always be either a function or a function reference
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+
+  const increaseByOne = () => setCounter(counter + 1)
+  
+  const setToZero = () => setCounter(0)
+
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+
+      <button onClick={setToZero}>
+        zero
+      </button>
+    </div>
   )
 }
-
+//he value of the onClick attribute is a variable containing 
+// a reference to a function
 export default App
