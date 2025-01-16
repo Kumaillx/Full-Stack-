8398a3f431d72690b5de600286c68a83c037a4f2
Syed M.Kumail Ali <101238514+bsee21068@users.noreply.github.com>
2025-01-16 18:22:06 +0500

    Asynchornous React state update

--- Part-1/Part-1d/src/App.jsx
+++ Part-1/Part-1d/src/App.jsx
@@ -4,40 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 const App = () => {
-  //arrays
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
-
   const [allClicks, setAll] = useState([])
-//in allClicks useState is initialized as an epmty array that stores everyclick
+//new total state that tells how many clicks we did
+  const [total, setTotal] = useState(0)
 
-//initially the allClicks array is empty when we click left or right button
-// We simply add Letter L or R into the empty array 
+
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+    
+    console.log('left before',left)
+    
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    
+    console.log('left after',left)
+    
+    setTotal(updatedLeft + right)
   }
-
-//(allClicks.concat('L') 
-//Combines two or more arrays. 
-//This method returns a new array without modifying any existing arrays.
-
-//Example:
-// const array1 = [1, 2, 3];
-// const array2 = [4, 5, 6];
-// const result = array1.concat(array2);
-// console.log(result); // Output: [1, 2, 3, 4, 5, 6]
-
 
   const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
-
-// We could have also used the push method instead but here we use the concat methods
-// Due to its property of merging arrays and values at the same time.
-// The React component such as APP should be mutated directly
-
+    setAll(allClicks.concat('R'));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  };
 
   return (
     <div>
@@ -45,15 +37,11 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-
       <p>{allClicks.join(' ')}</p>
+
+      <p>total {total}</p>
     </div>
   )
 }
-//{allClicks.join(' ')}
-//Adds all the elements of an array into a string,
-export default App
 
-// The main issue why we use concat instead of push is mutation
-// because it modifies the current array directly which is discouraged in React
-// You must make a copy or an instance and save inside those to take care of issues manually
+export default App
