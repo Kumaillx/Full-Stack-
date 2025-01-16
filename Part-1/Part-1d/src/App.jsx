fc9542ef5e76b234f82a2e0da2b150c6eb6de3c3
Syed M.Kumail Ali <101238514+bsee21068@users.noreply.github.com>
2025-01-16 18:09:24 +0500

    Using multiple arrays and concatination to merge values saved in an empty array

--- Part-1/Part-1d/src/App.jsx
+++ Part-1/Part-1d/src/App.jsx
@@ -4,28 +4,56 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  //arrays
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
-// The syntax '...clicks' is the spread operator. 
-// It is used here to create a shallow copy of the clicks object.
-//We can update state directly but its not a good practise
-//one should make a copy first to update the values
-const handleLeftClick = () =>
-  setClicks({ ...clicks, left: clicks.left + 1 })
+  const [allClicks, setAll] = useState([])
+//in allClicks useState is initialized as an epmty array that stores everyclick
 
-const handleRightClick = () =>
-  setClicks({ ...clicks, right: clicks.right + 1 })
+//initially the allClicks array is empty when we click left or right button
+// We simply add Letter L or R into the empty array 
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+//(allClicks.concat('L') 
+//Combines two or more arrays. 
+//This method returns a new array without modifying any existing arrays.
+
+//Example:
+// const array1 = [1, 2, 3];
+// const array2 = [4, 5, 6];
+// const result = array1.concat(array2);
+// console.log(result); // Output: [1, 2, 3, 4, 5, 6]
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+// We could have also used the push method instead but here we use the concat methods
+// Due to its property of merging arrays and values at the same time.
+// The React component such as APP should be mutated directly
+
 
   return (
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {right}
+
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
-
+//{allClicks.join(' ')}
+//Adds all the elements of an array into a string,
 export default App
+
+// The main issue why we use concat instead of push is mutation
+// because it modifies the current array directly which is discouraged in React
+// You must make a copy or an instance and save inside those to take care of issues manually
