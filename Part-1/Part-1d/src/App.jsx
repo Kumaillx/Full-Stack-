b447ee5d7018e47ee5eab58e1a9ea57e6fcb35c4
Syed M.Kumail Ali <101238514+bsee21068@users.noreply.github.com>
2025-01-16 16:04:02 +0500

    Using multiple pieces of states and familiarity with shallow copies and spread operators

--- Part-1/Part-1d/src/App.jsx
+++ Part-1/Part-1d/src/App.jsx
@@ -4,21 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 const App = () => {
-  
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-console.log(left)
-console.log(right)
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+// The syntax '...clicks' is the spread operator. 
+// It is used here to create a shallow copy of the clicks object.
+//We can update state directly but its not a good practise
+//one should make a copy first to update the values
+const handleLeftClick = () =>
+  setClicks({ ...clicks, left: clicks.left + 1 })
+
+const handleRightClick = () =>
+  setClicks({ ...clicks, right: clicks.right + 1 })
+
   return (
     <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>
-        left
-      </button>
-      <button onClick={() => setRight(right + 1)}>
-        right
-      </button>
-      {right}
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   )
 }
