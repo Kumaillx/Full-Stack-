395ac313c7eb0715d953ce873391c0b1450f81a3
Syed M.Kumail Ali <101238514+bsee21068@users.noreply.github.com>
2025-01-20 23:16:03 +0500

    passing event handlers to a child component

--- Part-1/Part-1d/src/App.jsx
+++ Part-1/Part-1d/src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
 
 const App = () => {
@@ -16,15 +21,9 @@ const App = () => {
   return (
     <div>
       {value}
-      <button onClick={() => setToValue(1000)}>
-        thousand
-      </button>
-      <button onClick={() => setToValue(0)}>
-        reset
-      </button>
-      <button onClick={() => setToValue(value + 1)}>
-        increment
-      </button>
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
