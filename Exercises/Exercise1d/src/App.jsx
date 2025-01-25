ff6c000c7982279bb5533174d97b8c8a7a8473f6
Syed M.Kumail Ali <101238514+bsee21068@users.noreply.github.com>
2025-01-25 21:45:05 +0500

    Making seperate component for Statistics and using if-else conditions for different displays

--- Exercises/Exercise1d/src/App.jsx
+++ Exercises/Exercise1d/src/App.jsx
@@ -1,15 +1,58 @@
 import { useState } from 'react'
 
-const App = () => {
-  // save clicks of each button to its own state
+const Statistics = (props) => {
+  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
+    
+    return (
+    <div>
+      <h2>{props.heading}</h2>
+      No Feedback Given
+    </div>
+    
+    );
+  } 
+  
+  else {
+    return (
+      <div>
+        <h2>{props.heading}</h2>
+        <div>
+        good {props.good}
+        </div>
+        
+        <div>
+          neutral {props.neutral}
+        </div>
+        <div>
+          bad {props.bad}
+        </div>
+          <div>
+            All {props.all}
+          </div>
+          <div>
+            Average {props.Average}
+          </div>
+          <div>
+            Positivity {props.Positivity}%
+          </div>
+      </div>      
+    );
+  };
+  }
+  
 
+const App = () => {
+// save clicks of each button to its own state
+  
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+ 
   const all = good + neutral + bad
   const Average = all > 0 ? (good - bad) / all : 0
   const Positivity = all > 0 ? (good / all) * 100 : 0
+
+  const heading = 'Statistics'
 
   return (
     <div>
@@ -27,18 +70,16 @@ const App = () => {
         Bad 
       </button>
        
-    
-      <div><h2> Statistics</h2></div>
+      <Statistics heading={heading} 
+          good = {good}
+          neutral = {neutral}
+          bad = {bad}
+          all = {all}
+          Average = {Average}
+          Positivity = {Positivity}
+
+      />
         
-        <div>good {good}</div>
-        
-        <div>neutral {neutral}</div>
-        
-        <div>bad {bad}</div>
-        
-        <div>all {all}</div>
-        <div>Average {Average}</div>
-        <div>Positivity {Positivity}%</div>
     </div>
   )
 }
