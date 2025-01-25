9602c65bdb1d910118a01f5b254afb2dc29deb4b
Syed M.Kumail Ali <101238514+bsee21068@users.noreply.github.com>
2025-01-25 22:11:26 +0500

    Refactoring and creating 2 components StaticLine and Button

--- Exercises/Exercise1d/src/App.jsx
+++ Exercises/Exercise1d/src/App.jsx
@@ -1,87 +1,73 @@
 import { useState } from 'react'
 
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <div>
+      {text} {value}
+    </div>
+  );
+};
+
 const Statistics = (props) => {
   if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
-    
-    return (
-    <div>
-      <h2>{props.heading}</h2>
-      No Feedback Given
-    </div>
-    
-    );
-  } 
-  
-  else {
     return (
       <div>
         <h2>{props.heading}</h2>
-        <div>
-        good {props.good}
-        </div>
-        
-        <div>
-          neutral {props.neutral}
-        </div>
-        <div>
-          bad {props.bad}
-        </div>
-          <div>
-            All {props.all}
-          </div>
-          <div>
-            Average {props.Average}
-          </div>
-          <div>
-            Positivity {props.Positivity}%
-          </div>
-      </div>      
+        No Feedback Given
+      </div>
     );
-  };
+  } else {
+    return (
+      <div>
+        <h2>{props.heading}</h2>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="All" value={props.all} />
+        <StatisticLine text="Average" value={props.Average} />
+        <StatisticLine text="Positivity" value={props.Positivity + '%'} />
+      </div>
+    );
   }
-  
+};
+
 
 const App = () => {
-// save clicks of each button to its own state
-  
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
- 
-  const all = good + neutral + bad
-  const Average = all > 0 ? (good - bad) / all : 0
-  const Positivity = all > 0 ? (good / all) * 100 : 0
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const heading = 'Statistics'
+  const all = good + neutral + bad;
+  const Average = all > 0 ? (good - bad) / all : 0;
+  const Positivity = all > 0 ? (good / all) * 100 : 0;
+
+  const heading = 'Statistics';
 
   return (
     <div>
-      <div><h1> Give Feedback</h1></div>
-      
-      <button onClick={()=>setGood(good + 1)}>
-        Good 
-      </button>
-      
-      <button onClick={()=>setNeutral(neutral + 1)}>
-        Neutral 
-      </button>
-      
-      <button onClick={()=>setBad(bad + 1)}>
-        Bad 
-      </button>
-       
-      <Statistics heading={heading} 
-          good = {good}
-          neutral = {neutral}
-          bad = {bad}
-          all = {all}
-          Average = {Average}
-          Positivity = {Positivity}
+      <div><h1>Give Feedback</h1></div>
 
+      <Button onClick={() => setGood(good + 1)} text="Good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="Bad" />
+
+      <Statistics
+        heading={heading}
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        Average={Average}
+        Positivity={Positivity}
       />
-        
     </div>
-  )
+  );
 }
+
 
 export default App
