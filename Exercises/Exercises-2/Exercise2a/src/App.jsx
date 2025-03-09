d1799f472bd811cb4cbf34a34a7b474f66897fbb
Syed M.Kumail Ali <101238514+bsee21068@users.noreply.github.com>
2025-03-09 22:04:18 +0500

    Exercise 2-a completed

--- Exercises/Exercises-2/Exercise2a/src/App.jsx
+++ Exercises/Exercises-2/Exercise2a/src/App.jsx
@@ -3,7 +3,8 @@ import Course from './components/Course'
 import React from 'react';
 
 const App = () => {
-  const course = {
+  const course = [
+  {
     id: 1,
     name: 'Half Stack application development',
     parts: [
@@ -21,16 +22,44 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
-  }
-    
-  return <Course course={course} />
+  },
+// Second Course 
+{
+  name: 'Node.js',
+  id: 2,
+  parts: [
+    {
+      name: 'Routing',
+      exercises: 3,
+      id: 1
+    },
+    {
+      name: 'Middlewares',
+      exercises: 7,
+      id: 2
+    }
+  ]
+}
+]
+// Here we use Id of each course to tell the computer which one to choose.
+  return (
+  <div>
+  {course.map(course=> (
+    <Course key={course.id} course={course}/>
+    ))}
+
+  </div>
+);
   
   // return <Course key={course.id} course={course} />
   
-
-}
-
+};
 
 export default App;
