a0dd7b7a41368676f4a184be8f07182fa107a43e
Syed M.Kumail Ali <101238514+bsee21068@users.noreply.github.com>
2025-03-28 23:04:21 +0500

    Forms practise

--- Part-2/Part-2b/src/App.jsx
+++ Part-2/Part-2b/src/App.jsx
@@ -1,32 +1,15 @@
-<<<<<<< HEAD
 //Forms
 //React Forms are the components 
 // used to collect and manage the user inputs.
 //user data , payments, handling authentications
-
-
-//states are built in objects
-import { useState } from 'react'
-import Note from './components/Note'
-
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
-//form
+
+
   const addNote = (event) => {
-    //prevents the page to reload as default settings
     event.preventDefault()
-    console.log('button clicked', event.target )
-
+    console.log('button clicked', event.target)
   }
-
-=======
-//states are built in objects
-import { useState } from 'react'
-import Note from './components/Note'
-
-const App = (props) => {
-  const [notes, setNotes] = useState([])
->>>>>>> 3970ab815dd34d1fbba0b284a12cd0f489ac544f
 
   return (
     <div>
@@ -36,19 +19,11 @@ const App = (props) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
-<<<<<<< HEAD
 
-        <form onSubmit={addNote}>
-          <input type="Add node" />
-          <button type='Submit'> save</button>
-        </form>
-
-
-
-=======
->>>>>>> 3970ab815dd34d1fbba0b284a12cd0f489ac544f
+      <form onSubmit={addNote}>
+        <input />
+        <button type="submit">save</button>
+      </form>   
     </div>
   )
 }
-
-export default App 
