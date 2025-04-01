0bfed27fc01fc7e82cef8b4052e030ceb21607f9
Syed M.Kumail Ali <101238514+bsee21068@users.noreply.github.com>
2025-04-01 20:39:38 +0500

    Exercise 2c completed for using effect hook, json server with different ports, and fetching, axios and promise.

--- Exercises-2/Exercise2c/src/App.jsx
+++ Exercises-2/Exercise2c/src/App.jsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Forms
+//React Forms are the components 
+// used to collect and manage the user inputs.
+//user data , payments, handling authentications
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Notes'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [notes, setNotes] = useState([])
+  //controlled components
+  const [newNote, setNewNote] = useState ('new note...')
+  const [ShowAll, setShowAll] = useState (true)
+
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3002/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
+  
+  console.log('render', notes.length,'notes')
+//const result = (5 > 3) ? "Yay" : "Boo";
+//condition is (5 > 3). This asks, “Is 5 bigger than 3?”
+// The answer is yes (true), because 5 is indeed bigger than 3.
+// Since it’s true, the computer picks "Yay" (the val1 part).
+// So, result gets filled with "Yay".
+//const result = condition ? val1 : val2
+  const notesToShow = ShowAll
+  ? notes
+  : notes.filter(note => note.important === true)
+
+  //The event handler is called every 
+  // time a change occurs in the input element.
+  //Handling of note changing
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+
+// add note variable (parameters event)
+// inside obj create
+// 3 features 
+// content newNote , imp randomly, id notes.length + 1
+
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+
+    const noteObj={
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+
+    setNotes(notes.concat(noteObj))
+    setNewNote('')
+
+    console.log(setNewNote)
+  }
 
   return (
-    <>
+    <div>
+      <h1>Notes</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={()=> setShowAll(!ShowAll)}>
+        show {ShowAll ? 'Important': 'All'}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+
+      <form onSubmit={addNote}>
+        <input 
+        value={newNote}
+        onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>   
+    </div>
   )
 }
+
 
 export default App
