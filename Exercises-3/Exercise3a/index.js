f1b6fc6a53b355a0b8f87851c52b83a5cf9ccd4c
Syed M.Kumail Ali <m.kumail80@gmail.com>
2025-07-29 20:20:52 +0500

    Got how now of what postman is and what rest is used for also tested the RESTful APIs

--- Exercises-3/Exercise3a/index.js
+++ Exercises-3/Exercise3a/index.js
@@ -4,34 +4,52 @@ const app = express()
 let notes = [
     {
         id: "1", // Changed to unique ID
-        content: "Banana can execute only JavaScript",
+        content: "Next is better than React",
         important: false
     },
     {
         id: "2", 
-        content: "Browser can execute only JavaScript", // Consider making content unique if needed
+        content: "Ronaldo is better than messi", // Consider making content unique if needed
         important: false
     },
     {
         id: "3",
-        content: "GET and POST are the most important methods of HTTP protocol",
+        content: "GET and POST are important methods for Express",
         important: true
     }
 ]
 
+// Data is fetched what to render on the root page
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
+// Looking at all notes (Json format)
 app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
+// Looking at a specific resourse from all notes (json format)
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
   const note = notes.find(note => note.id === id)
-  response.json(note)
+//   response.json(note)
+
+    //If note is present in JSON response then send it to client
+    if (note) { response.json(note) } 
+    //if note is not present just give 404 not found 
+    // error status to the client indicating the req doesnt exist
+    else { response.status(404).end() }
 })
+
+// Deleting a particular id 
+app.delete('/api/notes/:id', (request, response) => 
+{
+    const id = request.params.id
+    const note = notes.filter(note => note.id !== id)
+    response.status(204).end()
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {
