75d38ddd52d25380bb04aa3f711d7c657eeb62b4
Syed M.Kumail Ali <m.kumail80@gmail.com>
2025-07-31 18:49:57 +0500

    Added stuff into the db

--- Part-2/Part-2d/src/services/Notes.js
+++ Part-2/Part-2d/src/services/Notes.js
@@ -1,9 +1,13 @@
+
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = 'http://localhost:3001/api/notes'
 
 const getAll = () => {
-  return axios.get(baseUrl)
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
 }
+
+
 
 const create = newObject => {
   return axios.post(baseUrl, newObject)
@@ -13,9 +17,5 @@ const update = (id, newObject) => {
   return axios.put(`${baseUrl}/${id}`, newObject)
 }
 
-export default { 
-  getAll: getAll, 
-  create: create, 
-  update: update 
-}
 
+export default { getAll, create, update }
